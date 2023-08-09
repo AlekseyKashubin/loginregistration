dca675d13ac0812debc5898c999bba502af9881c
AlekseyKashubin <Akashubin@yahoo.com>
2023-08-09 14:14:06 -0700

    update

--- Models/User.cs
+++ Models/User.cs
@@ -18,6 +18,7 @@ public class User
 
     [Required]
     [EmailAddress]
+    [UniqueEmail]
     public string Email { get; set; }
 
     [Required]
@@ -40,23 +41,25 @@ public class UniqueEmailAttribute : ValidationAttribute
 {
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-    	// Though we have Required as a validation, sometimes we make it here anyways
-    	// In which case we must first verify the value is not null before we proceed
-        if(value == null)
+        // Though we have Required as a validation, sometimes we make it here anyways
+        // In which case we must first verify the value is not null before we proceed
+        if (value == null)
         {
-    	    // If it was, return the required error
+            // If it was, return the required error
             return new ValidationResult("Email is required!");
         }
-    
-    	// This will connect us to our database since we are not in our Controller
+
+        // This will connect us to our database since we are not in our Controller
         MyContext db = (MyContext)validationContext.GetService(typeof(MyContext));
         // Check to see if there are any records of this email in our database
-    	if(db.Users.Any(e => e.Email == value.ToString()))
+        if (db.Users.Any(e => e.Email == value.ToString()))
         {
-    	    // If yes, throw an error
+            // If yes, throw an error
             return new ValidationResult("Email must be unique!");
-        } else {
-    	    // If no, proceed
+        }
+        else
+        {
+            // If no, proceed
             return ValidationResult.Success;
         }
     }
